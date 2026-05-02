9172c06b8e3aae10c22e6237501fc9a6bf270e85
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-02 15:00:24 +0300

    Add Navbar component with navigation links

--- client-app/src/components/Navbar.jsx
+++ client-app/src/components/Navbar.jsx
@@ -1,1 +1,10 @@
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/login">Login</Link> | <Link to="/dashboard">Dashboard</Link>
+    </nav>
+  );
+}
+export default Navbar;

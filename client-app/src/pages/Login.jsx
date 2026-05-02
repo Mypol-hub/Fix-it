43f042c9ed7d94b8d42648de776d361f05f96539
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-02 14:26:26 +0300

    Implement basic Login component

---
+++ client-app/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <div>
+      <h2>Client Login</h2>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+export default Login;

493ea1bcd50acddf5ed2da908ed369f362ed7e6f
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-04 14:17:39 +0300

    Add Navbar and new routes for Feedback and RequestForm

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,15 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Feedback from "./pages/Feedback";
+import RequestForm from "./pages/RequestForm";
 
 function App() {
   return (
     <Router>
+      {/* Navbar always visible */}
+      <Navbar />
+
+      {/* Page routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/request" element={<RequestForm />} />
       </Routes>
     </Router>
   );

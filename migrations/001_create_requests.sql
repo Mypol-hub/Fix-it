219ef25bbfef54fc5970d4b0c6ac7b652a676b3c
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-05 11:04:52 +0300

    Delete migrations directory

--- migrations/001_create_requests.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE requests (
-  id SERIAL PRIMARY KEY,
-  customer_name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  item_name TEXT NOT NULL,
-  problem_description TEXT,
-  created_at TIMESTAMP DEFAULT NOW()
-);
--- migrations/002_create_feedbacks.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE feedbacks (
-  id SERIAL PRIMARY KEY,
-  email TEXT NOT NULL,
-  feedback TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
-);

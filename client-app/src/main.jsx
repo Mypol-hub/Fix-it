fc6b43c3b04d37dae4b3b3446db08ffe76c52640
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-10 15:26:44 +0300

    Switch from BrowserRouter to HashRouter
    Replaced BrowserRouter with HashRouter for routing.

--- client-app/src/main.jsx
+++ client-app/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Must be HashRouter
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Fix-it">
+    <HashRouter> 
+      {/* Note: You don't need basename="/Fix-it" with HashRouter! */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );

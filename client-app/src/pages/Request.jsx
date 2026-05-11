17e6fba6d0fd02d38bc56e9616b659e871035742
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-11 20:26:24 +0300

    Import CSS file for Request component

--- client-app/src/pages/Request.jsx
+++ client-app/src/pages/Request.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../supabaseClient";
+import Request from "./Request.css";
 
 function Request() {
   const [customerName, setCustomerName] = useState("");

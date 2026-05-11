002945bba688943c78d71f0c575b0b8d2c1b4258
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-11 12:41:34 +0300

    Add Signup route and component to App

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Request from "./pages/Request";
 import Feedback from "./pages/Feedback";
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/request" element={<Request />} />
         <Route path="/feedback" element={<Feedback />} />

efc15e1a97ac2e6045d92d688ea4206b09e9fd26
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-12 11:49:39 +0300

    Update base path in Vite configuration

--- admin-app/vite.config.js
+++ admin-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Fix-it-admin/', // This MUST match your repository name or subfolder
+  base: '/Fix-it/admin/', // This MUST match your repository name or subfolder
 })

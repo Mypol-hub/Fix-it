adedf53fed850ce1ec6dba3020bfc4a9f5624f28
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-14 19:33:27 +0300

    Update Supabase key environment variable

--- admin-app/src/supabaseClient.js
+++ admin-app/src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Pulling from .env for security
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);

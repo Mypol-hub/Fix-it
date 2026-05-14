5147e471e7e63e181c80d6ae98dc3b60f6d8f973
Mypol-hub <maetis009@gmail.com>
2026-05-14 14:45:53 +0000

    update changes

--- client-app/src/supabaseClient.js
+++ client-app/src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://grfbratwsbjgfevtymac.supabase.co";
-const supabaseKey = "sb_publishable_vO6B1olb8cXsaqJQliKRZQ_2eflMa0o"; // from Supabase dashboard → Project Settings → API
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);

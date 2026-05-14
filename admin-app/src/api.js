e0b7c16261498f1fa3f150a212278cf3ce844677
Mypol-hub <maetis009@gmail.com>
2026-05-14 15:52:24 +0000

    updte chnges

--- admin-app/src/api.js
+++ admin-app/src/api.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from './supabaseClient';
 
 // Vite uses import.meta.env to access .env variables
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 

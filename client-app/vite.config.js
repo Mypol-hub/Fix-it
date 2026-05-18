c06fc84af8970926a4ac0c7a1be654a088fd8dba
Mypol-hub <74507245+Mypol-hub@users.noreply.github.com>
2026-05-18 19:01:10 +0300

    Comment out base and server settings in vite.config.js
    Comment out base and server configurations for Vite.

--- client-app/vite.config.js
+++ client-app/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Use the repository name so assets load correctly on GitHub Pages
-  base: '/Fix-it/', 
+  /*base: '/Fix-it/', */
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  server: {
-    historyApiFallback: true,
   }
+  /*server: {
+    historyApiFallback: true,
+  }*/
 })
